dd836552c92f44b3718758b8199604620bf5dea1
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-10-03 18:43:43 +0700

    Update demo_test.js

--- demo_test.js
+++ demo_test.js
@@ -1,6 +1,3 @@
-const { I } = require("codeceptjs");
-const crypto = require("crypto");
-const Joi = require("joi");
 Feature("DEMO");
 
 let authToken; // Biến để lưu trữ token
@@ -21,8 +18,7 @@ Before(async ({ I }) => {
 });
 
 After(async ({ I }) => {
-  //clg
-  console.log("Log: clear data example")
+  //
 });
 
 //Kết thúc hook chung cho tất cả các scenario trong feature
@@ -32,31 +28,24 @@ Scenario("POST", async ({ I }) => {
   formData.append(
     "blog",
     JSON.stringify({
-      title: "This is a blog Test title",
-      headerTop: "This is a blog Test header",
-      contentTop: "This is a blog Test content",
-      headerMiddle: "This is a blog Test header",
-      contentMiddle: "This is a blog DET estMO content",
+      title: "This is a blog DEMO title V2333",
+      headerTop: "This is a blog DEMO header",
+      contentTop: "This is a blog DEMO content",
+      headerMiddle: "This is a blog DEMO header",
+      contentMiddle: "This is a blog DEMO content",
     })
   );
   formData.append("headerImage", "");
   formData.append("bodyImage", "");
   const response = await I.sendPostRequest("/api/blogs", formData);
+
+  console.log(response)
   blogId = response.data.data.id;
-
-  I.seeResponseContainsJson({ 
-    message: 'Created blog successfully',
-    statusCode: 201,
-    data: {
-      title: "This is a blog Test title"
-    }
-  });
-
   I.seeResponseCodeIsSuccessful();
 });
 
 Scenario("GET", async ({ I }) => {
-  await I.sendGetRequest("/api/blogs");
+  await I.sendGetRequest("/api/blogs/");
   I.seeResponseCodeIsSuccessful();
 });
 
@@ -65,7 +54,7 @@ Scenario("PUT", async ({ I }) => {
   formData.append(
     "blog",
     JSON.stringify({
-      title: "This is update for a blog title",
+      title: "This is a blog title",
       headerTop: "This is a blog DEMO PUT header",
       contentTop: "This is a blog DEMO PUT content",
       headerMiddle: "This is a blog DEMO header",
@@ -76,22 +65,17 @@ Scenario("PUT", async ({ I }) => {
   formData.append("bodyImage", "");
   const response = await I.sendPutRequest("/api/blogs/" + blogId, formData);
   I.seeResponseCodeIsSuccessful();
-
-  I.seeResponseContainsJson({ 
-    data: {
-      title: "This is update for a blog title"
-    }
-  });
+  console.log(response.data.data);
 });
 
 Scenario("DELETE", async ({ I }) => {
-  const response = await I.sendDeleteRequest("/api/blogs/" + blogId);
-  I.seeResponseCodeIsSuccessful();
+
 });
 
 Scenario("GET BY ID", async ({ I }) => {
   const response = await I.sendGetRequest("/api/blogs/" + blogId);
-  I.seeResponseCodeIs(404);
+  I.seeResponseCodeIsSuccessful();
+  console.log(response.data.data);
 });
 
 Scenario("GET package BY ID", async ({ I }) => {
